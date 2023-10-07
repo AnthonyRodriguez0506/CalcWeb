548ea89f4d8a21b7b2595989334b1254c29098f3
Anthony Rodríguez <anthonygithub097@gmail.com>
2023-10-07 18:41:11 -0400

    Correct functions and button result

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function valor (x){
-    document.getElementById("display").value += x;
-
+function valueButtons(x) {
+    document.getElementById('display').value += x;
+}
+function clearDisplay(y) {
+    document.getElementById('display').value = y;
+}
+function resultDisplay() {
+    var result = eval(document.getElementById('display').value);
+    document.getElementById('display').value = result;
 }
